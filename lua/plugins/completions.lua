f7ceb528b8764bcae777a23ff77c25c9c9dcc60f
dontuo <bench.oleg@gmail.com>
2024-09-05 20:06:37 +0300

    fixed "bug"

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -23,7 +23,7 @@ return {
         },
         window = {
           completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
+          --documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -34,7 +34,6 @@ return {
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
         }, {
           { name = "buffer" },
         }),
